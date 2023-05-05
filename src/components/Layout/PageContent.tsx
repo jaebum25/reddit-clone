de28e8c4b644570b3e6fed638926cf994ef3adc4
Jaebum Lee <brian.lee025@gmail.com>
2023-05-05 16:49:43 -0400

    create post community with image in db

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -11,19 +11,17 @@ const PageContent:React.FC<PageContentProps> = ({ children }) => {
     <Flex 
       justify='center' 
       p='16px 0px' 
-      border='1px solid red'
     >
       <Flex 
         width='95%' 
         justify='center' 
         maxWidth='860px' 
-        border='1px solid green'
       >
         {/* LHS */}
         <Flex 
           direction='column' 
           width={{ base: '100%', md: '65%' }} 
-          mr={{ base: 0, md: 6 }} border='1px solid blue'
+          mr={{ base: 0, md: 6 }}
         >
           {children && children[0 as keyof typeof children]}
         </Flex>
@@ -32,7 +30,7 @@ const PageContent:React.FC<PageContentProps> = ({ children }) => {
         <Flex 
           direction='column' 
           display={{ base: 'none', md: 'flex' }} 
-          flexGrow={1} border='1px solid orange'
+          flexGrow={1}
         >
           {children && children[1 as keyof typeof children]}
         </Flex>
