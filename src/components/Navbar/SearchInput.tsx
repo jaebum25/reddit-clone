a89b97fb51799f0d4cc051f754535523bc7e92ef
Jaebum Lee <brian.lee025@gmail.com>
2023-05-15 02:20:16 -0400

    fix build errors 1

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -1,40 +1,38 @@
-import { SearchIcon } from '@chakra-ui/icons';
-import { Flex, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
-import { User } from 'firebase/auth';
-import React from 'react';
+import { SearchIcon } from "@chakra-ui/icons";
+import { Flex, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { User } from "firebase/auth";
+import React from "react";
 
 type SearchInputProps = {
   user?: User | null;
 };
 
-const SearchInput:React.FC<SearchInputProps> = ({ user }) => {
-  
+const SearchInput: React.FC<SearchInputProps> = ({ user }) => {
   return (
-    <Flex maxWidth={user ? 'auto' : '600px'} flexGrow={1} mr={2} align='center'>
+    <Flex maxWidth={user ? "auto" : "600px"} flexGrow={1} mr={2} align="center">
       <InputGroup>
-        <InputLeftElement
-          pointerEvents='none'
-          children={<SearchIcon color='gray.400' mb={1} />}
-        />
-        <Input 
-          placeholder='Search Reddit' 
-          fontSize='10pt' 
-          _placeholder={{ color: 'gray.500'}} 
+        <InputLeftElement pointerEvents="none">
+          <SearchIcon color="gray.400" mb={1} />
+        </InputLeftElement>
+        <Input
+          placeholder="Search Reddit"
+          fontSize="10pt"
+          _placeholder={{ color: "gray.500" }}
           _hover={{
-            bg: 'white',
-            border: '1px_solid',
-            borderColor: 'blue.500',
+            bg: "white",
+            border: "1px_solid",
+            borderColor: "blue.500",
           }}
           _focus={{
-            outline: 'none',
-            border: '1px solid',
-            borderColor: 'blue.500',
+            outline: "none",
+            border: "1px solid",
+            borderColor: "blue.500",
           }}
-          height='34px'
-          bg='gray.50'
+          height="34px"
+          bg="gray.50"
         />
       </InputGroup>
     </Flex>
-  )
-}
+  );
+};
 export default SearchInput;
