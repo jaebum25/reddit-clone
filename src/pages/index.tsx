efe452edb3a8d98409137b35f7ffd458a32f89e9
Jaebum Lee <brian.lee025@gmail.com>
2023-05-15 01:37:44 -0400

    additional community components unproductive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,6 +23,8 @@ import PostItem from "../components/Posts/PostItem";
 import CreatePostLink from "../components/Community/CreatePostLink";
 import useCommunityData from "../hooks/useCommunityData";
 import Recommendations from "../components/Community/Recommendations";
+import Premium from "../components/Community/Premium";
+import PersonalHome from "../components/Community/PersonalHome";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -160,9 +162,11 @@ export default function Home() {
           </Stack>
         )}
       </>
-      <>
+      <Stack spacing={5}>
         <Recommendations />
-      </>
+        <Premium />
+        <PersonalHome />
+      </Stack>
     </PageContent>
   );
 }
