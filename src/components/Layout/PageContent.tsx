af4ed86e3c25c2acad2b1b863748d88f493310df
Jaebum Lee <brian.lee025@gmail.com>
2023-05-15 02:49:54 -0400

    PageContent children prop fix

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -1,41 +1,33 @@
-import { Flex } from '@chakra-ui/react';
-import React from 'react';
+import { Flex } from "@chakra-ui/react";
+import React, { ReactNode } from "react";
 
 type PageContentProps = {
-  
+  children: ReactNode;
 };
 
-const PageContent:React.FC<PageContentProps> = ({ children }) => {
-  
+const PageContent: React.FC<PageContentProps> = ({ children }) => {
   return (
-    <Flex 
-      justify='center' 
-      p='16px 0px' 
-    >
-      <Flex 
-        width='95%' 
-        justify='center' 
-        maxWidth='860px' 
-      >
+    <Flex justify="center" p="16px 0px">
+      <Flex width="95%" justify="center" maxWidth="860px">
         {/* LHS */}
-        <Flex 
-          direction='column' 
-          width={{ base: '100%', md: '65%' }} 
+        <Flex
+          direction="column"
+          width={{ base: "100%", md: "65%" }}
           mr={{ base: 0, md: 6 }}
         >
           {children && children[0 as keyof typeof children]}
         </Flex>
 
         {/* RHS */}
-        <Flex 
-          direction='column' 
-          display={{ base: 'none', md: 'flex' }} 
+        <Flex
+          direction="column"
+          display={{ base: "none", md: "flex" }}
           flexGrow={1}
         >
           {children && children[1 as keyof typeof children]}
         </Flex>
       </Flex>
     </Flex>
-  )
-}
+  );
+};
 export default PageContent;
