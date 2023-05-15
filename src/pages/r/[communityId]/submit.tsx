384345d6287b6b6f1160395ffc305407fb08dd83
Jaebum Lee <brian.lee025@gmail.com>
2023-05-15 02:38:33 -0400

    submite typo fix

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -3,13 +3,13 @@ import About from "@/src/components/Community/About";
 import PageContent from "@/src/components/Layout/PageContent";
 import NewPostForm from "@/src/components/Posts/NewPostForm";
 import { auth } from "@/src/firebase/clientApp";
-import useCommunityData from "@/src/hooks/useCommunityData";
+import useCommunityData from "../../../hooks/useCommunityData";
 import { Box, Text } from "@chakra-ui/react";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRecoilValue } from "recoil";
 
-const submitPostPage: React.FC = () => {
+const SubmitPostPage: React.FC = () => {
   const [user] = useAuthState(auth);
   // const communityStateValue = useRecoilValue(communityState);
   const { communityStateValue } = useCommunityData();
@@ -35,4 +35,4 @@ const submitPostPage: React.FC = () => {
     </PageContent>
   );
 };
-export default submitPostPage;
+export default SubmitPostPage;
