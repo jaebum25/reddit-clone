3f8a2d1328f48609afc67777f3b344d34295c2b1
Jaebum Lee <brian.lee025@gmail.com>
2023-05-15 15:43:05 -0400

    index query  build fix

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -5,11 +5,12 @@ import Header from "@/src/components/Community/Header";
 import NotFound from "@/src/components/Community/NotFound";
 import PageContent from "@/src/components/Layout/PageContent";
 import Posts from "@/src/components/Posts/Posts";
-import { firestore } from "@/src/firebase/clientApp";
+import { auth, firestore } from "@/src/firebase/clientApp";
 import { doc, getDoc } from "firebase/firestore";
-import { GetServerSideProps, NextPage } from "next";
+import { GetServerSideProps, NextPage, NextPageContext } from "next";
 import React, { useEffect } from "react";
-import { useSetRecoilState } from "recoil";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import safeJsonStringify from "safe-json-stringify";
 
 type CommunityPageProps = {
@@ -17,7 +18,10 @@ type CommunityPageProps = {
 };
 
 const CommunityPage: NextPage<CommunityPageProps> = ({ communityData }) => {
-  const setCommunityStateValue = useSetRecoilState(communityState);
+  const [user, loadingUser] = useAuthState(auth);
+
+  const [communityStateValue, setCommunityStateValue] =
+    useRecoilState(communityState);
 
   useEffect(() => {
     setCommunityStateValue((prev) => ({
@@ -46,9 +50,9 @@ const CommunityPage: NextPage<CommunityPageProps> = ({ communityData }) => {
   );
 };
 
-export const getServerSideProps: GetServerSideProps<
-  CommunityPageProps
-> = async (context) => {
+export async function getServerSideProps(context: NextPageContext) {
+  console.log("GET SERVER SIDE PROPS RUNNING");
+
   try {
     const communityDocRef = doc(
       firestore,
@@ -62,7 +66,7 @@ export const getServerSideProps: GetServerSideProps<
           ? JSON.parse(
               safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
             )
-          : null,
+          : "",
       },
     };
   } catch (error) {
@@ -73,6 +77,6 @@ export const getServerSideProps: GetServerSideProps<
       },
     };
   }
-};
+}
 
 export default CommunityPage;
